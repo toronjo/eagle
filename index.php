8bffecfb408586c13220442b6e456ee0bcd10536
Alex G <jalexg@gmail.com>
2018-01-07 20:07:59 -0600

    resuming

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <html>
 <head>
 <script>
-function showProjects(str) {
+function showByPM(str) {
     if (str == "") {
         document.getElementById("txtHint").innerHTML = "";
         return;
@@ -18,16 +18,44 @@ function showProjects(str) {
                 document.getElementById("txtHint").innerHTML = this.responseText;
             }
         };
-        xmlhttp.open("GET","listpms.php?q="+str,true);
+        xmlhttp.open("GET","show_by_pm.php?q="+str,true);
         xmlhttp.send();
     }
 }
+
+function showByPfM(str) {
+    if (str == "") {
+        document.getElementById("txtHint").innerHTML = "";
+        return;
+    } else {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttp = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                document.getElementById("txtHint").innerHTML = this.responseText;
+            }
+        };
+        xmlhttp.open("GET","show_by_pfm.php?q="+str,true);
+        xmlhttp.send();
+    }
+}
+
+
 </script>
 </head>
 <body>
 
 <form>
-  <input type="text" name="pm" onkeypress="showProjects(this.value)">
+  <strong> PM: </strong>
+  <input type="text" name="pm" onkeypress="showByPM(this.value)">
+  &nbsp;&nbsp;&nbsp;&nbsp;
+  <strong> PfM: </strong>
+  <input type="text" name="pm" onkeypress="showByPfM(this.value)">
 </form>
 <br>
 <div id="txtHint"><b>Person info will be listed here...</b></div>
